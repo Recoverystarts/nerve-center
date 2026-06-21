9ca0d0b3af8b1560273228d42ebb49233835f548
Recoverystarts <dmccorriston2222@proton.me>
2026-06-21 10:49:13 -0600

    Add core source files and configs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NerveCenterScene from './components/scene/NerveCenterScene'
+
+function App() {
+  return (
+    <div className="w-full h-screen overflow-hidden bg-[#02050a]">
+      <NerveCenterScene />
+    </div>
+  )
+}
+
+export default App
